d75e116f33cae593a8291b91855231fb14023da6
matheus <matheusgolminif@gmail.com>
2023-04-12 17:09:54 -0300

    :hammer: initial config service http

---
+++ service-http/src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('users')
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getHello(): string {
+    return this.userService.getHello();
+  }
+}
